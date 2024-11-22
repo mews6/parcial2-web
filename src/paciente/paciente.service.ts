8064e0f0cc9d5dcb16e91583aa96fae96c628988
Jaime Torres <jatber.mrowr@gmail.com>
2024-11-22 09:14:31 -0500

    add addMedicoToPaciente

--- src/paciente/paciente.service.ts
+++ src/paciente/paciente.service.ts
@@ -5,13 +5,18 @@ import { PacienteEntity } from './paciente.entity';
 
 import { BusinessError, 
     BusinessLogicException } from 'src/shared/errors/business-errors';
-
+import { MedicoEntity } from 'src/medico/medico.entity';
+import { MedicoService } from 'src/medico/medico.service';
 
 @Injectable()
-export class MedicoService {
+export class PacienteService {
     constructor(
         @InjectRepository(PacienteEntity)
-        private readonly pacienteRepository: Repository<PacienteEntity>
+        private readonly pacienteRepository: Repository<PacienteEntity>,
+
+        @InjectRepository(MedicoEntity)
+        private readonly medicoRepository: Repository<MedicoEntity>
+
     ) {}
     
     async findAll(): Promise<PacienteEntity[]> {
@@ -44,5 +49,28 @@ export class MedicoService {
             BusinessError.NOT_FOUND
         ); 
         await this.pacienteRepository.remove(paciente);
+    }
+
+    async addMedicoToPaciente(pacienteId: number, medicoId:number){
+        const paciente: PacienteEntity = await this.pacienteRepository.findOne({where: { id:pacienteId }});
+        const medico: MedicoEntity = await this.medicoRepository.findOne({where: { id:medicoId }});
+
+        if (!paciente)
+            throw new BusinessLogicException(
+                'The patient with that given id was not found',
+                BusinessError.NOT_FOUND
+            ); 
+        if (!medico)
+            throw new BusinessLogicException(
+                'The medic with that given id was not found',
+                BusinessError.NOT_FOUND
+            ); 
+        if (paciente.medicos.length >= 5)
+            throw new BusinessLogicException(
+            'The current patient has reached the maximum number of possible medics.',
+            BusinessError.PRECONDITION_FAILED
+        )
+
+        paciente.medicos.push(medico);
     }
 }
